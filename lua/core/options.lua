cbf5081b02efc29d7c5f0b5f89899b2bc02ddd79
Ashley Felton <ashley@ropable.com>
2024-08-09 11:59:41 +0800

    Tabstop options

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -8,11 +8,11 @@ opt.relativenumber = true
 opt.number = true
 
 -- Tabs & Indentation
-opt.tabstop = 4
-opt.shiftwidth = 4
+opt.tabstop = 2
+opt.shiftwidth = 2
 opt.expandtab = true
 opt.autoindent = true
-vim.bo.softtabstop = 4
+vim.bo.softtabstop = 2
 
 -- Line Wrapping
 opt.wrap = false
