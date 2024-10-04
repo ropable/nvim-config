5e07f81118621be727110882f79be1764e09baa3
Ashley Felton <ashley@ropable.com>
2024-10-04 08:42:26 +0800

    Longer timeout for formatters.

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,6 +1,10 @@
 return {
   "stevearc/conform.nvim",
   opts = {
+    default_format_opts = {
+      -- Allow formatters 10 seconds to work.
+      timeout_ms = 10000,
+    },
     formatters_by_ft = {
       css = { "prettier" },
       htmldjango = { "djlint" },
