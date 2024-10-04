80dfe492f52dbe56d672cd9434417818f3da854d
Ashley Felton <ashley@ropable.com>
2024-10-04 08:58:56 +0800

    Added nvim-lint config.

---
+++ lua/plugins/nvim-lint.lua
@@ -0,0 +1,13 @@
+-- https://github.com/LazyVim/LazyVim/discussions/4094#discussioncomment-10178217
+local HOME = os.getenv("HOME")
+return {
+  "mfussenegger/nvim-lint",
+  optional = true,
+  opts = {
+    linters = {
+      ["markdownlint-cli2"] = {
+        args = { "--config", HOME .. "/.markdownlint-cli2.yaml", "--" },
+      },
+    },
+  },
+}
